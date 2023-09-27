db32b76a09c8e66d7fceac617348f90abab2789b
ravi barla <ravibarla1@gmail.com>
2023-09-27 11:11:52 +0530

    setting up server

--- server.js
+++ server.js
@@ -12,5 +12,4 @@ res.send("welcome to e-commerce app")
 //4. specify the port
 const port=3200
 
-server.listen(port)
-console.log("server is running in 3200 port")
+server.listen(port,()=>console.log("server is running in 3200 port"))
