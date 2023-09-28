fbb2c047264d9101aa59478a8cc3415a2fbc07e5
ravi barla <ravibarla1@gmail.com>
2023-09-28 10:57:53 +0530

    some changes

--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-//1. import express
-import express from "express"
-
-//2. create server
-const server=express()
-
-//3. default request handler
-server.get("/",(req,res)=>{
-res.send("welcome to e-commerce app")
-})
-
-//4. specify the port
-const port=3200
-
-server.listen(port,()=>console.log("server is running in 3200 port"))
-=======
 //1.import express
 import express from "express";
 import productRouter from "./src/features/product/product.routes.js";
@@ -34,4 +17,3 @@ server.get("/", (req, res) => {
 //4. create port
 const PORT = 3200;
 server.listen(PORT, () => console.log("app running in port ", PORT));
->>>>>>> 08dad8ae65ca9f1f1637132156a83e5a1aaa6891
