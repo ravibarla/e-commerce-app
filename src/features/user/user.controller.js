c6a512f693bf1cc6e3792cf328e0b5f5c377da9d
ravi barla <ravibarla1@gmail.com>
2023-10-01 23:04:16 +0530

    bug fixing

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -7,11 +7,16 @@ export default class UserController {
     this.userRepository = new UserRepository();
   }
   async signUp(req, res) {
-    const { name, email, password, type } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = new UserModel(name, email, hashedPassword, type);
-    await this.userRepository.signUp(user);
-    res.status(201).send(user);
+    try {
+      const { name, email, password, type } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new UserModel(name, email, hashedPassword, type);
+      await this.userRepository.signUp(user);
+      res.status(201).send(user);
+    } catch (err) {
+      console.log(err);
+      res.status(200).send(user);
+    }
   }
   async signIn(req, res) {
     try {
